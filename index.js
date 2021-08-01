df2fec9ad5a035449a431a7fb559835e096bc2a2
Michael Harris <xero5k@yahoo.com>
2021-08-01 17:48:48 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ] 
+
+function superbowlWin(arg){
+    const goal = arg.find(element => element.result === "W");
+    //const found = array1.find(element => element > 10);
+    if(goal != undefined){
+        //console.log(arg.result, arg.year)
+        return goal.year;
+    }  
+    else {
+        return goal;
+    }
+}
+
+console.log(record.find(superbowlWin));
 
